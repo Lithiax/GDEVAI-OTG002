0aa8aa745b84ee7be4048ef1eed05a1468016506
Kaell_O <mikaell.olondriz@benilde.edu.ph>
2020-09-14 23:35:22 +0800

    Module 1 and fixed gitignore

---
+++ Module 1 - Pet/Assets/Scripts/PetMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetMovement : MonoBehaviour
+{
+    public Transform goal;
+    float rotSpeed = 3;
+    void LateUpdate()
+    {
+        Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+        Vector3 direction = lookAtGoal - transform.position;
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+
+
+        if (Vector3.Distance(lookAtGoal, transform.position) > 2)
+        {
+            transform.position = Vector3.Lerp(transform.position, goal.position, Time.deltaTime);
+        }
+    }
+}
---
+++ Module 1 - Pet/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float moveSpeed = 8;
+
+    void LateUpdate()
+    {
+
+        //mouse position ray cast code from https://www.youtube.com/watch?v=lkDGk3TjsIE 
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, this.transform.position);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(pointToLook);
+        }
+
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput * moveSpeed;
+
+        if(Vector3.Distance(cameraRay.GetPoint(rayLength), this.transform.position) > 0.5)
+            transform.Translate(moveVelocity * Time.deltaTime);
+    }
+}
