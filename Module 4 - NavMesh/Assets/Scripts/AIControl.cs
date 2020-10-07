a7cba3cee4aed02d852c575d69aad5a4f56bd894
Lithiax <kaell0325@gmail.com>
2020-10-07 17:58:40 +0800

    mod 3/4

---
+++ Module 4 - NavMesh/Assets/Scripts/AIControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIControl : MonoBehaviour
+{
+    public Transform player;
+    float speed;
+    UnityEngine.AI.NavMeshAgent agent;
+    void Start()
+    {
+        this.agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        speed = agent.speed;
+    }
+    void Update() 
+    {
+        agent.SetDestination(player.position);
+
+        if(Vector3.Distance(this.transform.position, player.position) < 8.0f)
+        {
+            agent.speed = 0;
+        }
+        else
+        {
+            agent.speed = speed;
+        }
+    }
+}
---
+++ Module 4 - NavMesh/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        transform.position += transform.forward * (z * speed) * Time.deltaTime;
+        transform.position += transform.right * (x * speed) * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            speed = 15f;
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            speed = 5f;
+    }
+}
