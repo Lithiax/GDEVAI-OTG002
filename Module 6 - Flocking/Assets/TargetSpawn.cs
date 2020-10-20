59e0757901bd22ee7e650ac366fd96118646c357
Lithiax <kaell0325@gmail.com>
2020-10-20 21:53:55 +0800

    module 6

---
+++ Module 6 - Flocking/Assets/TargetSpawn.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TargetSpawn : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject monster;
+    GameObject[] agents;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("agent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                Instantiate(target, hit.point, target.transform.rotation);
+                foreach (GameObject a in agents)
+                {
+                    a.GetComponent<AIControl>().DetectFlockObstacle(hit.point);
+                }
+            }
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                Instantiate(monster, hit.point, monster.transform.rotation);
+                foreach (GameObject a in agents)
+                {
+                    a.GetComponent<AIControl>().DetectFleeObstacle(hit.point);
+                }
+            }
+        }
+    }
+    
+    public void Restart()
+    {
+        SceneManager.LoadScene("Starter");
+    }
+}
